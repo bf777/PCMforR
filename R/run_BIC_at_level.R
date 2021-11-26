5a73c6812ccdad085b508dc5fd4a19316e06e861
Brandon Forys <bforys@gmail.com>
2021-11-25 17:55:13 -0800

    Completed initial pass at main training/weighting loop

--- R/run_BIC_at_level.R
+++ R/run_BIC_at_level.R
@@ -52,9 +52,6 @@ run_BIC_at_level <- function(train_data, POIs_list, POI_names, POIs_to_use, anal
     # Calculate all BIC scores at level
     BICs <- unlist(lapply(POI_names, calc_lm_BIC, POIs_to_use, train_data))
 
-    # Record all BIC scores at vertical level in BIC log
-    BIC_log[vert_level_idx, 1:length(POIs_list)] <- BICs
-
     # find which BIC is the best, save it in best_BICs, and index its position
     min_BIC <- min(BICs)
     best_BICs <- c(best_BICs, min_BIC)
@@ -63,39 +60,41 @@ run_BIC_at_level <- function(train_data, POIs_list, POI_names, POIs_to_use, anal
     # horiz_level is list of equivalent BIC scores at horizontal level
     horiz_level <- vector()
 
-    # Make a vector of all BICs except for the min one (for comparison)
-    other_BICs <- BICs[-min_BIC_idx]
+    # add POI with new best BIC to POIs_to_use so that it can be included in
+    # next level of BIC
+    # Record all BIC scores at vertical level in BIC log
+    BIC_log[vert_level_idx, 1:length(POIs_list)] <- BICs
+    POIs_to_use <- c(POIs_to_use, POI_names[min_BIC_idx])
+    BIC_log[vert_level_idx, length(POIs_list) + 1] <- min_BIC_idx
+    BIC_log_list <- c(BIC_log_list, BIC_log)
 
-    # TODO: figure out how to best compare best BIC to other BICs
+    # If BIC improved vs. previous best BIC more than criterion, check if BIC is better than other BICS +/- 2
 
-    # If BIC improved vs. criterion, check if BIC is better than other BICS +/- 2
-    criterion <- 2
-    for (other_BIC in other_BICs) {
-      if (abs(min_BIC - other_BIC) > criterion) {
+    # If we're not on the first vertical level (we have a prior BIC to compare to),
+    # compare BICs
+    if (vert_level_idx >= 2) {
+      criterion <- 2
+      previous_min_BIC <- best_BICs[length(best_BICs) - 1]
+      if (abs(min_BIC - previous_min_BIC) > criterion) {
         # If best BIC already found in horizontal level, check if it's the same
         # horizontal level
-        if (length(horiz_level) > 0) {
+        if (length(horiz_level) == 0) {
           if (length(duplicated(BICs))) {
-            equivalent_BICs <- which(duplicated(BICS) == BICs)
-            # If we have equivalency, record indices of equivalent BICs in horiz_level
+            equivalent_BICs <- which(duplicated(BICs) == BICs)
+            # If we have equivalency and we're not already in a horizontal level,
+            # record indices of equivalent BICs in horiz_level
             horiz_level <- c(horiz_level, equivalent_BICs)
             horiz_level_list <- c(horiz_level_list, horiz_level)
             # Continue at next vertical level
           }
         }
-          # If not, continue directly to next vertical level and save current BIC
-          # log table
+        # If not, continue directly to next vertical level and save current BIC
+        # log table
 
-          # add POI with new best BIC to POIs_to_use so that it can be included in
-          # next level of BIC
-          POIs_to_use <- c(POIs_to_use, POI_names[min_BIC_idx])
-          BIC_log[vert_level_idx, length(POIs_list) + 1] <- min_BIC_idx
-          BIC_log_list <- c(BIC_log_list, BIC_log)
-          vert_level_idx <- vert_level_idx + 1
         } else {
           # If BIC not improved vs criterion:
           # Check if min BIC < best BIC
-          if (min_BIC < best_BICs[length(best_BICs) - 1]) {
+          if (min_BIC < previous_min_BIC) {
             best_BIC <- min_BIC
         }
 
@@ -112,33 +111,49 @@ run_BIC_at_level <- function(train_data, POIs_list, POI_names, POIs_to_use, anal
           }
           }
         }
+      # If the horizontal level at the current vertical level is not empty,
+      # continue BIC
+      horiz_level_idx <- horiz_level_idx + 1
     }
-    # If the horizontal level at the current vertical level is not empty,
-    # continue BIC
-    horiz_level_idx <- horiz_level_idx + 1
 
-    # If BIC not improved vs. previous and empty level found, finish training
-
-    # Weight POIs_list
-    # POIs_to_add_weighting <- sapply(POIs_to_use, paste, collapse = " + ")
-    # formula_to_use_weighting <- paste0('train_data ~ ', POIs_to_add_weighting)
-    # weighted_POIs_lm <- lm(formula_to_use_weighting)
-    # weighted_POIs <- summary(weighted_POIs_lm)$coefficients[1,1]
-
-    # Export BIC logs
-    # data_logger(BIC_log_list, 'BIC_log', analysis_type, ROI, output_dir)
-
-    # Continue to testing
-    # return(weighted_POIs)
+  # If we're on the first level, go to the next level without recording BIC
+  vert_level_idx <- vert_level_idx + 1
   }
+  # If BIC not improved vs. previous and empty level found, finish training
+
+  # Weight POIs_list
+  POIs_to_add_weighting <- paste(POIs_to_use, collapse = " + ")
+  formula_to_use_weighting <- paste0('train_data ~ ', POIs_to_add_weighting)
+  POI_weights_lm <- lm(as.formula(formula_to_use_weighting))
+  POI_weights <- summary(POI_weights_lm)$coefficients[,1]
+
+  weighted_POIs_list <- vector()
+
+  for (POI_to_weight_idx in seq(1,length(POIs_list))) {
+    POI_to_weight <- unlist(POIs_list[POI_to_weight_idx])
+    POI_to_weight_name <- POI_names[POI_to_weight_idx]
+    if(POI_to_weight_name %in% POIs_to_use) {
+      POI_to_weight <- POI_to_weight * POI_weights[POI_to_weight_name]
+      weighted_POIs_list <- c(weighted_POIs_list, POI_to_weight)
+    }
+  }
+  print(weighted_POIs_list)
+
+  # Export BIC logs
+  # print(data.frame(BIC_log_list))
+  # data_logger(BIC_log_list, 'BIC_log', analysis_type, ROI, output_dir)
+
+  # Continue to testing
+  # return(weighted_POIs_list)
 }
 
 calc_lm_BIC <- function(POI, POIs_to_use, train_data) {
   # set up lm with train data ~ current POI + all identified POIs
-  POIs_to_add <- sapply(POIs_to_use, paste, collapse = " + ")
+  # POIs_to_add <- sapply(POIs_to_use, paste, collapse = " + ")
 
   # If more than one POI has been identified, add identified POIs to lm
-  if (length(POIs_to_add) != 0) {
+  if (length(POIs_to_use) != 0) {
+    POIs_to_add <- paste(POIs_to_use, collapse = " + ")
     formula_to_use <- paste0('train_data ~ ', POI, ' + ', POIs_to_add)
   } else {
     formula_to_use <- paste0('train_data ~ ', POI)
